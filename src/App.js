015e23c19d75703774697ead693ffaa90dd41d91
sumeshsuse <sumeshsuseelan.techie@gmail.com>
2024-01-11 17:31:07 +0100

    2nd commit!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <div>Up and Running!</div>
+     <div>made changes Up and Running!</div>
     </div>
   );
 }
